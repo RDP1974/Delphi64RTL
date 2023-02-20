1a4bcd364bbb02c9e3997c83ab8ee3d860668790
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2023-02-20 16:16:45 +0100

    Add files via upload
    updated webbroker deflate helper, this should be reliable
    average json table shrink 4x
    5x faster than default gzip iis10

--- RDPWebBroker64.pas
+++ RDPWebBroker64.pas
@@ -4,6 +4,7 @@ unit RDPWebBroker64;
 // for better performances put RDPMM64 as first unit clause in project source
 // define SEAZLIB for compression realtime speed up
 // 26 Febr 2020 buffer len check
+// 20 Febr 2023 updated zlibdeflate for correct mime
 
 {$DEFINE SEAZLIB}
 
@@ -15,98 +16,62 @@ type
   TWBHelper = class helper for TWebResponse
   public
     procedure ZlibDeflate; overload;
-    procedure ZlibDeflate(const Src:TMemoryStream); overload;
+ //   procedure ZlibDeflate(const Src: TMemoryStream); overload;
   end;
 
 implementation
 
-{$IFDEF SEAZLIB}
 uses RDPZlib64;
-{$ELSE}
-uses System.Zlib;
-{$ENDIF}
 
-procedure TWBHelper.ZlibDeflate;
+procedure TWBHelper.ZlibDeflate; // compress utf-8 text and json
 var
   ZBuff: TMemoryStream;
   Src, Dst: TBytes;
+  Cl: string;
 begin
-  if ContentStream = nil then // allora accedere a content string
+  Cl := Lowercase(ContentType);
+  if ((Pos('text', Cl) > 0) or (Pos('json', Cl) > 0) and (Pos('utf-8', Cl) > 0)) then
   begin
-    if (Length(Content) > 1500) and (Length(Content) < 10000000) then
+    if ContentStream = nil then // use content string
     begin
-      Src := TEncoding.UTF8.GetBytes(Content);
-      SeaZlib.Compress(Src, Dst);
-      ZBuff := TMemoryStream.Create;
-      ZBuff.Write(Dst, Length(Dst));
-      ContentStream := ZBuff;
-      ContentStream.Seek(0, 0);
-      ContentEncoding := 'deflate';
-      ContentLength := ZBuff.Size;
-      (* Src := TEncoding.UTF8.GetBytes(Content);
-        ZBuffStr:=TMemoryStream.Create;
-        ZBuffStr.Write(Src, Length(Src));
-        ZBuffStr.Seek(0,0);
+      if (Length(Content) > 1500) and (Length(Content) < 10000000) then // 10MB
+      begin
+        Src := TEncoding.UTF8.GetBytes(Content);
+        SeaZlib.Compress(Src, Dst);
         ZBuff := TMemoryStream.Create;
-        SeaZlib.CompressStream(ZBuffStr, ZBuff, Z_BEST_SPEED_AC);
-        ZBuffStr.Free;
+        ZBuff.Write(Dst, Length(Dst));
         ContentStream := ZBuff;
-        ContentStream.Seek(0, 0);
+        ContentStream.Seek(0,0);
         ContentEncoding := 'deflate';
-        ContentLength := ZBuff.Size; *)
+        ContentLength := ContentStream.Size;
+      end;
+    end
+    else if (ContentStream.Size > 1500) and (ContentStream.Size < 10000000) then // 10MB
+    begin
+      ZBuff := TMemoryStream.Create;
+      SeaZlib.CompressStream(ContentStream, ZBuff, Z_BEST_SPEED_AC);
+      ContentStream.Size := ZBuff.Size;
+      ContentStream.Write(ZBuff, ZBuff.Size);
+      ContentEncoding := 'deflate';
+      ContentLength := ContentStream.Size;
+      ZBuff.Free;
     end;
-  end
-  else if (ContentStream.Size > 1500) and (ContentStream.Size < 10000000) then
-  begin
-    ZBuff := TMemoryStream.Create;
-    ContentStream.Seek(0, 0);
-{$IFDEF SEAZLIB}
-    SeaZlib.CompressStream(ContentStream, ZBuff, Z_BEST_SPEED_AC);
-{$ELSE}
-    ZCompressStream(ContentStream, ZBuff, zcFastest);
-{$ENDIF}
-    ContentStream.Free;
-    ContentStream := ZBuff;
-    ContentStream.Seek(0, 0);
-    ContentEncoding := 'deflate';
-    ContentLength := ZBuff.Size;
   end;
 end;
 
-procedure TWBHelper.ZlibDeflate(const Src:TMemoryStream);
-{$IFDEF SEAZLIB}
-var
-  Helper:TMemoryStream;
-begin
-  if (Src.Size > 1500) and (Src.Size < 10000000) then
-  begin
-    Src.Seek(0, 0);
-    Helper:=TMemoryStream.Create;
-    SeaZlib.CompressStream(Src, Helper, Z_BEST_SPEED_AC);
-    ContentStream := Helper;
-    ContentStream.Seek(0, 0);
-    ContentEncoding := 'deflate';
-    ContentLength := Helper.Size;
-  end;
-  {$ELSE}
-var
-  HelperTZ:TZCompressionStream;
-  Helper:TMemoryStream;
-begin
-  if (Src.Size > 1500) and (Src.Size < 10000000) then
-  begin
-    Src.Seek(0, 0);
-    Helper:=TMemoryStream.Create;
-    HelperTZ:=TZCompressionStream.Create(Helper, zcFastest, -15);
-    HelperTZ.CopyFrom(Src, Src.Size);
-    HelperTZ.Free;
-    ContentStream := Helper;
-    ContentStream.Seek(0, 0);
-    ContentEncoding := 'deflate';
-    ContentLength := Helper.Size;
-  end;
-{$ENDIF}
-
-end;
+//procedure TWBHelper.ZlibDeflate(const Src: TMemoryStream);
+//var
+//  Helper: TMemoryStream;
+//begin
+//  if (Src.Size > 1500) and (Src.Size < 10000000) then // 10MB
+//  begin
+//    Src.Seek(0, 0);
+//    Helper := TMemoryStream.Create;
+//    SeaZlib.CompressStream(Src, Helper, Z_BEST_SPEED_AC);
+//    ContentStream := Helper;
+//    ContentEncoding := 'deflate';
+//    ContentLength := Helper.Size;
+//  end;
+//end;
 
 end.
