bfe0e908070f991cdaf3dd433f213eef52ad5a08
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2025-04-09 18:14:51 +0200

    Add files via upload

--- RDPSimd64.pas
+++ RDPSimd64.pas
@@ -4,6 +4,7 @@ unit RDPSimd64;
 // rtl api patches with Intel IPP performance libraries
 // 8 ago 2024 updated to intel ipp v2021.12, visual c++ v19.29.30154
 // seartl.dll md5 6f35648fbf2b386e3129ec82bb12d30d size 200704
+// 9 apr 2025 rem fillchar and move in case of Delphi 12.x (comes with asm optimized x64 functions)
 
 interface
 
@@ -184,8 +185,10 @@ end;
 
 procedure PatchRTL64;
 begin
+{$IF CompilerVersion < 36.0}
   RedirectCode(@System.Move, @Move2);
   RedirectCode(OrigFillchar, @Fillchar2);
+{$IFEND}
   RedirectCode(RetrievePosRawAddr, @PosRaw);
   RedirectCode(RetrievePosWideAddr, @PosWide);
   RedirectCode(RetrievePosUnicodeAddr, @PosUnicode);
