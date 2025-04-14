f3d954a46ad1d5eba6aec0715cfddd25928ee6a7
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2025-04-14 17:27:30 +0200

    Add files via upload

---
+++ RDPQueue64.pas
@@ -0,0 +1,22 @@
+unit RDPQueue64;
+// Roberto Della Pasqua www.dellapasqua.com
+// 14 apr 2024 thread safe concurrent queue from Intel One Api v2022.1
+
+interface
+
+uses Windows;
+
+const
+  QPARDLL = 'SeaQPar.dll';
+
+function CreateQueue:pointer; stdcall; external QPARDLL name 'CreateQueue';
+function PopFromQueue(queue: pointer; ptr: Pointer): LongBool; stdcall; external QPARDLL name 'PopFromQueue';
+function IsQueueEmpty(queue: pointer): LongBool; stdcall; external QPARDLL name 'IsQueueEmpty';
+procedure PushToQueue(queue: pointer; ptr: Pointer); stdcall; external QPARDLL name 'PushToQueue';
+procedure FreeQueue(queue: pointer); stdcall; external QPARDLL name 'FreeQueue';
+
+implementation
+
+
+end.
+
