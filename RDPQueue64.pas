cd02019ea2af379c7fc5278e19f1320f50d71a9d
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2025-04-17 16:17:40 +0200

    Update RDPQueue64.pas

--- RDPQueue64.pas
+++ RDPQueue64.pas
@@ -10,7 +10,7 @@ const
   QPARDLL = 'SeaQPar.dll';
 
 function CreateQueue:pointer; stdcall; external QPARDLL name 'CreateQueue';
-function PopFromQueue(queue: pointer; ptr: Pointer): LongBool; stdcall; external QPARDLL name 'PopFromQueue';
+function PopFromQueue(queue: pointer; ptr: pointer): LongBool; stdcall; external QPARDLL name 'PopFromQueue';
 function IsQueueEmpty(queue: pointer): LongBool; stdcall; external QPARDLL name 'IsQueueEmpty';
 procedure PushToQueue(queue: pointer; ptr: Pointer); stdcall; external QPARDLL name 'PushToQueue';
 procedure FreeQueue(queue: pointer); stdcall; external QPARDLL name 'FreeQueue';
