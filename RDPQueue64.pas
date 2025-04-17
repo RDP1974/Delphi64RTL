f57749a42e543f11c4e5d6277bc8dc9aaecbcd29
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2025-04-17 16:18:23 +0200

    Update RDPQueue64.pas

--- RDPQueue64.pas
+++ RDPQueue64.pas
@@ -12,7 +12,7 @@ const
 function CreateQueue: pointer; stdcall; external QPARDLL name 'CreateQueue';
 function PopFromQueue(queue: pointer; ptr: pointer): LongBool; stdcall; external QPARDLL name 'PopFromQueue';
 function IsQueueEmpty(queue: pointer): LongBool; stdcall; external QPARDLL name 'IsQueueEmpty';
-procedure PushToQueue(queue: pointer; ptr: Pointer); stdcall; external QPARDLL name 'PushToQueue';
+procedure PushToQueue(queue: pointer; ptr: pointer); stdcall; external QPARDLL name 'PushToQueue';
 procedure FreeQueue(queue: pointer); stdcall; external QPARDLL name 'FreeQueue';
 
 implementation
