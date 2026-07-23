a41e704c4b5056481932bec1300171b2f17cd75c
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2026-07-23 15:52:13 +0200

    Add files via upload

--- RDPQueue64.pas
+++ RDPQueue64.pas
@@ -2,13 +2,20 @@ unit RDPQueue64;
 // Roberto Della Pasqua www.dellapasqua.com
 // 14 apr 2025 thread safe concurrent queue from Intel One Api v2022.1
 // 20 jun 2026 update intel oneapi 2023.1
+// 23 july 2026 linux static library tbb and libstd++ (latest gcc and tbb at the time)
 
 interface
 
+{$IFDEF WIN64}
 uses Windows;
-
 const
   QPARDLL = 'SeaQPar.dll';
+{$ENDIF}
+
+{$IFDEF LINUX}
+const
+  QPARDLL = 'libseaqpar.so';
+{$ENDIF}
 
 function CreateQueue: pointer; stdcall; external QPARDLL name 'CreateQueue';
 function PopFromQueue(queue: pointer; ptr: pointer): LongBool; stdcall; external QPARDLL name 'PopFromQueue';
